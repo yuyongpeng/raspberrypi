fba8ede9aa3fd03a9fa0833cde4839c1f89fa9cf
yuyongpeng <yuyongpeng@modernmedia.com.cn>
2018-04-16 15:57:37 +0800

    添加了rabbitmq的监听和web socket的处

--- websocket-server.js
+++ websocket-server.js
@@ -1,4 +1,9 @@
- var WebSocketServer = require('ws').Server,
+function sleep(d){
+  for(var t = Date.now();Date.now() - t <= d;);
+}
+
+var amqp = require('amqp');  
+var WebSocketServer = require('ws').Server,
 wss = new WebSocketServer({ port: 8181 });
 wss.on('connection', function (ws) {
     console.log('client connected');
@@ -6,5 +11,41 @@ wss.on('connection', function (ws) {
         console.log('fffff');
         console.log(message);
     });
+    for(var t = Date.now();Date.now() - t <= 1000;);
+    console.log('kkkkkkk')
+    //ws.send('ffffff')
 });
 
+sleep(5000); //当前方法暂停5秒
+//wss.send('pop')
+console.log('0000000')
+console.log(wss.clients.length)
+wss.clients.forEach(function each(client) {
+      console.log('1111111')
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        console.log('222222')
+        client.send('fffffkkkkkk');
+      }
+});
+
+var connection = amqp.createConnection({url: "amqp://hardchain:pswHd@localhost:5672/%2F"});
+// Wait for connection to become established.
+connection.on('ready', function () {
+    var callbackCalled = false;
+    exchange = connection.exchange('xxx', {type: 'direct',autoDelete:false});
+    connection.queue("electron3",{autoDelete:false}, function(queue){
+        queue.bind('xxx','electron3', function() {
+            //exchange.publish('electron3', 'this is message is testing ......');
+             callbackCalled = true;
+        });
+
+        queue.subscribe(function (message) {
+          console.log('At 5 second recieved message is:'+ message.data);
+          wss.clients.forEach(function each(client){
+              console.log('11111')
+              client.send('fffffak')
+          });
+        });
+
+    });
+});
